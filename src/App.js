c0a1a1f98240d2d3bc53f6476afca613412ee308
Teemu Taskula <teemu.taskula@aalto.fi>
2017-06-02 23:50:15 +0300

    Added basic structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,73 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+} from 'react-router-dom'
+
+
 import logo from './logo.svg';
 import './App.css';
+
+import Home from './views/Home';
+import Dashboard from './views/Dashboard';
+import Settings from './views/Settings';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Awesomefy!</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <div>
+            <Navbar>
+              <Title>Quantum App</Title>
+              <NavItems>
+                <Link to='/'>Home</Link>
+                <Link to='/dashboard'>Dashboard</Link>
+                <Link to='/settings'>Settings</Link>
+              </NavItems>
+            </Navbar>
+
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/dashboard' component={Dashboard} />
+              <Route path='/settings' component={Settings} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
 }
+
+const Navbar = styled.div`
+  height: 60px;
+  width: 100%;
+  padding: 0px 32px;
+  display: flex;
+  align-items: center;
+  background-color: tomato;
+  box-shadow: 0px 0px 18px rgba(0,0,0,0.4);
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  text-transform: uppercase;
+  color: #fff;
+  margin-right: 40px;
+`;
+
+const NavItems = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  & a {
+    margin-right: 16px;
+    text-decoration: none;
+    color: #fff;
+  }
+`;
 
 export default App;
--- src/electron-starter.js
+++ src/electron-starter.js
@@ -16,12 +16,13 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  mainWindow.loadURL('http://localhost:3000');
-  // mainWindow.loadURL(url.format({
-  //   pathname: path.join(__dirname, 'index.html'),
-  //   protocol: 'file:',
-  //   slashes: true
-  // }))
+  const startUrl = process.env.ELECTRON_START_URL || url.format({
+    pathname: path.join(__dirname, '/../build/index.html'),
+    protocol: 'file:',
+    slashes: true
+  });
+  
+  mainWindow.loadURL(startUrl);
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools()

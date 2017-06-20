43e503703095a8038946c0581af1750a14f38cf6
Teemu Taskula <teemu.taskula@aalto.fi>
2017-06-20 23:02:53 +0300

    Added youtube apis and created initial app layout

--- src/App.js
+++ src/App.js
@@ -1,73 +1,128 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Switch,
-} from 'react-router-dom'
-
-
-import logo from './logo.svg';
 import './App.css';
 
-import Home from './views/Home';
-import Dashboard from './views/Dashboard';
-import Settings from './views/Settings';
+import withApis from './components/withApis';
+import Sidebar from './views/Sidebar';
+import Navbar from './views/Navbar';
+import CurrentPlaylist from './views/CurrentPlaylist';
+import ControlBar from './views/ControlBar';
 
 class App extends Component {
+  state = {
+    searchTerm: '',
+  }
+
+  componentDidMount() {
+    this.player = new window.YT.Player('player', {
+      height: '360',
+      width: '640',
+    });
+  }
+
+  play = () => {
+    this.player.playVideo();
+  };
+
+  pause = () => {
+    this.player.pauseVideo();
+  };
+
+  loadVideo = (videoId) => {
+    this.player.loadVideoById({ videoId });
+  };
+
+  handleSearch = ({ target }) => {
+    const searchTerm = target.value;
+
+    this.setState({ searchTerm });
+
+    if (searchTerm.length > 3) {
+      this.props.youtubeApi.search.list({
+        q: searchTerm,
+        part: 'snippet',
+      }).execute(({ result }) => {
+        const video = result.items[0];
+        this.loadVideo(video.id.videoId);
+      });
+    }
+  };
+
   render() {
     return (
-      <div className="App">
-        <Router>
-          <div>
-            <Navbar>
-              <Title>Quantum App</Title>
-              <NavItems>
-                <Link to='/'>Home</Link>
-                <Link to='/dashboard'>Dashboard</Link>
-                <Link to='/settings'>Settings</Link>
-              </NavItems>
-            </Navbar>
+      <AppWrapper className='App'>
+        <MainWrapper>
+          <Sidebar />
 
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route path='/dashboard' component={Dashboard} />
-              <Route path='/settings' component={Settings} />
-            </Switch>
-          </div>
-        </Router>
-      </div>
+          <MainContent>
+            <Navbar />
+            <CurrentPlaylist />
+          </MainContent>
+        </MainWrapper>
+
+        <ControlBar />
+      </AppWrapper>
     );
   }
 }
+/*
+<Search
+  value={this.state.searchTerm || ''}
+  onChange={this.handleSearch}
+/>
 
-const Navbar = styled.div`
-  height: 60px;
-  width: 100%;
-  padding: 0px 32px;
+<Controls>
+  <ControlIcon
+    className='mdi mdi-play-circle-outline'
+    onClick={this.play}
+  />
+  <ControlIcon
+    className='mdi mdi-pause-circle-outline'
+    onClick={this.pause}
+  />
+</Controls>
+*/
+
+const AppWrapper = styled.div`
+  height: 100vh;
+  widht: 100vw;
   display: flex;
-  align-items: center;
-  background-color: tomato;
-  box-shadow: 0px 0px 18px rgba(0,0,0,0.4);
+  flex-direction: column;
 `;
 
-const Title = styled.h1`
-  font-size: 24px;
-  text-transform: uppercase;
-  color: #fff;
-  margin-right: 40px;
-`;
-
-const NavItems = styled.div`
+const MainWrapper = styled.div`
+  flex: 1;
   display: flex;
   flex-direction: row;
-
-  & a {
-    margin-right: 16px;
-    text-decoration: none;
-    color: #fff;
-  }
 `;
 
-export default App;
+const MainContent = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`;
+
+// const Controls = styled.div`
+//   display: flex;
+//   flex-direction: row;
+//   align-items: center;
+//   width: 100%;
+//   margin-top: 100px;
+// `;
+
+// const ControlIcon = styled.i`
+//   font-size: 60px;
+//   color: #222;
+//   margin: 0 16px;
+// `;
+
+// const Search = styled.input`
+//   font-size: 16px;
+//   color: #222;
+//   padding: 8px 16px;
+//   border: 1px solid #ccc;
+//   background-color: #f5f5f5;
+//   border-radius: 6px;
+// `;
+
+export default withApis(App);
